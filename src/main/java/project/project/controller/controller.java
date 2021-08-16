98e64165bfd68172871e4aeb02eecd0da120e395
chan <kscks1234@kakao.com>
2021-08-17 00:09:18 +0900

    annotation or DI

--- src/main/java/project/project/controller/controller.java
+++ src/main/java/project/project/controller/controller.java
@@ -3,6 +3,8 @@ package project.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class controller {
@@ -11,5 +13,35 @@ public class controller {
     public String hello(Model model){
         model.addAttribute("data","hello");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name",name);
+            return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    //html 바디부에 직접 내가 넣어줄때 사용하는거
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ src/main/java/project/project/service/MemberService.java
@@ -0,0 +1,45 @@
+package project.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.project.domain.Member;
+import project.project.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원가입
+    public Long join(Member member){
+        //중복이름이 있는 회원은 안됨
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
